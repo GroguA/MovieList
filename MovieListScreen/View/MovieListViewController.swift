d2ad0ade1325561b23529761e4dbb64ec15af0e6
GroguA <117662071+GroguA@users.noreply.github.com>
2024-06-13 17:14:41 +0300

    Create project

---
+++ MovieListScreen/View/MovieListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MovieListViewController.swift
+//  MovieList
+//
+//  Created by Александра Сергеева on 13.06.2024.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
