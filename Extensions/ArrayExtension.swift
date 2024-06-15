66e7fd4ae4394f888066b3ec992548a2b3a74457
GroguA <117662071+GroguA@users.noreply.github.com>
2024-06-15 15:31:13 +0300

    Add extension for array

--- Extensions/ArrayExtension.swift
+++ Extensions/ArrayExtension.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+extension Array where Element: Hashable {
+    func unique() -> [Element] {
+        var unique = Set<Element>()
+        return filter { unique.insert($0).inserted }
+    }
+}
