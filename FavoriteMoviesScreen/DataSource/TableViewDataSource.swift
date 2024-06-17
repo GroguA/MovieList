4e0fc389171ec94220311a4d4904f53f15cf6e84
GroguA <117662071+GroguA@users.noreply.github.com>
2024-06-17 21:22:33 +0300

    Add data source for favorite movies screen

--- FavoriteMoviesScreen/DataSource/TableViewDataSource.swift
+++ FavoriteMoviesScreen/DataSource/TableViewDataSource.swift
@@ -5,4 +5,44 @@
 //  Created by Александра Сергеева on 17.06.2024.
 //
 
-import Foundation
+import UIKit
+
+enum TableSection {
+    case main
+}
+
+final class FavoriteMoviesTableViewDataSource {
+    typealias DataSource = UITableViewDiffableDataSource<TableSection, FavoriteMovieModel>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<TableSection, FavoriteMovieModel>
+    
+    var dataSource: DataSource?
+    
+    private var tableView: UITableView
+    
+    var presenter: IFavoriteMoviesPresenter?
+    
+    init(tableView: UITableView) {
+        self.tableView = tableView
+    }
+    
+    func makeDataSource() {
+        dataSource = DataSource(
+            tableView: tableView,
+            cellProvider: { (tableView, indexPath, movie) -> UITableViewCell? in
+                guard let cell = tableView.dequeueReusableCell(
+                    withIdentifier: FavoriteMoviesTableViewCell.identifier,
+                    for: indexPath
+                ) as? FavoriteMoviesTableViewCell else { return nil }
+                
+                cell.fillCell(with: movie)
+                return cell
+            })
+    }
+    
+    func applySnapshot(with movies: [FavoriteMovieModel], animatingDifferences: Bool = true) {
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(movies)
+        dataSource?.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
+}
