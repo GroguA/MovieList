2c3586cdbaf45029d5cb23b5cc92ef517c1564e8
GroguA <117662071+GroguA@users.noreply.github.com>
2024-06-17 15:37:46 +0300

    Refactor network layer

--- MovieListScreen/NetworkLayer/MovieNetworkService.swift
+++ MovieListScreen/NetworkLayer/MovieNetworkService.swift
@@ -18,107 +18,83 @@ final class MovieNetworkService {
     private let session = URLSession.shared
     private let mapper = JsonDecoderWrapper()
     
+    private func makeURLRequest(endpoint: String, queryItems: [URLQueryItem]) -> URLRequest? {
+        var urlComponents = URLComponents(string: endpoint)
+        urlComponents?.queryItems = queryItems
+        
+        guard let url = urlComponents?.url else {
+            return nil
+        }
+        return URLRequest(url: url)
+    }
+
     private func makePopularMoviesRequest(page: Int) -> URLRequest? {
-        var baseComponent = URLComponents(string: "https://api.themoviedb.org/3/movie/popular")
-        let apiKey = URLQueryItem(name: "api_key", value: apiKey)
-        let language = URLQueryItem(name: "language", value: "en-US")
-        let page = URLQueryItem(name: "page", value: "\(page)")
+        let endpoint = "https://api.themoviedb.org/3/movie/popular"
+        let apiKeyItem = URLQueryItem(name: "api_key", value: apiKey)
+        let languageItem = URLQueryItem(name: "language", value: "en-US")
+        let pageItem = URLQueryItem(name: "page", value: "\(page)")
         
-        let parametrs = [apiKey, language, page]
-        
-        baseComponent?.queryItems = parametrs
-        
-        guard let baseUrlComponent = baseComponent?.url else {
-            return nil
-        }
-        return URLRequest(url: baseUrlComponent)
+        let queryItems = [apiKeyItem, languageItem, pageItem]
+        return makeURLRequest(endpoint: endpoint, queryItems: queryItems)
     }
-    
+
     private func makeMovieRequestByQuery(_ query: String) -> URLRequest? {
-        var baseComponent = URLComponents(string: "https://api.themoviedb.org/3/search/movie")
-        let apiKey = URLQueryItem(name: "api_key", value: apiKey)
-        let language = URLQueryItem(name: "language", value: "en-US")
-        let query = URLQueryItem(name: "query", value: query)
+        let endpoint = "https://api.themoviedb.org/3/search/movie"
+        let apiKeyItem = URLQueryItem(name: "api_key", value: apiKey)
+        let languageItem = URLQueryItem(name: "language", value: "en-US")
+        let queryItem = URLQueryItem(name: "query", value: query)
         
-        let parametrs = [apiKey, language, query]
-        
-        baseComponent?.queryItems = parametrs
-        
-        guard let baseUrlComponent = baseComponent?.url else {
-            return nil
-        }
-        return URLRequest(url: baseUrlComponent)
+        let queryItems = [apiKeyItem, languageItem, queryItem]
+        return makeURLRequest(endpoint: endpoint, queryItems: queryItems)
     }
+
 }
 
 extension MovieNetworkService: IMovieNetworkService {
+    func fetchMovies(with request: URLRequest, completion: @escaping (Result<[MovieScheme], Error>) -> Void) {
+        let completionHandler: (Data?, URLResponse?, Error?) -> Void = { data, response, error in
+            if let error = error {
+                completion(.failure(NetworkErrors.parseError(message: error.localizedDescription)))
+                return
+            }
+
+            guard let data = data, let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(NetworkErrors.invalidResponse))
+                return
+            }
+
+            switch httpResponse.statusCode {
+            case 200..<300:
+                self.mapper.parse(MoviesResponseScheme.self, from: data) { result in
+                    switch result {
+                    case .success(let model):
+                        completion(.success(model.results))
+                    case .failure(let error):
+                        completion(.failure(error))
+                    }
+                }
+            default:
+                completion(.failure(NetworkErrors.statusCodeError(message: HTTPURLResponse.localizedString(forStatusCode: httpResponse.statusCode))))
+            }
+        }
+        let task = session.dataTask(with: request, completionHandler: completionHandler)
+        task.resume()
+    }
+
     func getPopularMovies(page: Int, completion: @escaping (Result<[MovieScheme], Error>) -> Void) {
         guard let request = makePopularMoviesRequest(page: page) else {
             completion(.failure(NetworkErrors.invalidRequest))
             return
         }
-        
-        let completionHandler: (Data?, URLResponse?, Error?) -> Void = { data, response, error in
-            if let error = error {
-                completion(.failure(NetworkErrors.parseError(message: error.localizedDescription)))
-                return
-            }
-            
-            guard let data = data, let httpResponse = response as? HTTPURLResponse else {
-                completion(.failure(NetworkErrors.invalidResponse))
-                return
-            }
-            
-            switch httpResponse.statusCode {
-            case 200..<300:
-                self.mapper.parse(MoviesResponseScheme.self, from: data) { result in
-                    switch result {
-                    case .success(let model):
-                        completion(.success(model.results))
-                    case .failure(let error):
-                        completion(.failure(error))
-                    }
-                }
-            default:
-                completion(.failure(NetworkErrors.statusCodeError(message: HTTPURLResponse.localizedString(forStatusCode: httpResponse.statusCode))))
-            }
-        }
-        let task = session.dataTask(with: request, completionHandler: completionHandler)
-        task.resume()
+        fetchMovies(with: request, completion: completion)
     }
-    
+
     func searchMovieByQuery(_ query: String, completion: @escaping (Result<[MovieScheme], Error>) -> Void) {
         guard let request = makeMovieRequestByQuery(query) else {
             completion(.failure(NetworkErrors.invalidRequest))
             return
         }
-        
-        let completionHandler: (Data?, URLResponse?, Error?) -> Void = { data, response, error in
-            if let error = error {
-                completion(.failure(NetworkErrors.parseError(message: error.localizedDescription)))
-                return
-            }
-            
-            guard let data = data, let httpResponse = response as? HTTPURLResponse else {
-                completion(.failure(NetworkErrors.invalidResponse))
-                return
-            }
-            
-            switch httpResponse.statusCode {
-            case 200..<300:
-                self.mapper.parse(MoviesResponseScheme.self, from: data) { result in
-                    switch result {
-                    case .success(let model):
-                        completion(.success(model.results))
-                    case .failure(let error):
-                        completion(.failure(error))
-                    }
-                }
-            default:
-                completion(.failure(NetworkErrors.statusCodeError(message: HTTPURLResponse.localizedString(forStatusCode: httpResponse.statusCode))))
-            }
-        }
-        let task = session.dataTask(with: request, completionHandler: completionHandler)
-        task.resume()
+        fetchMovies(with: request, completion: completion)
     }
+
 }
