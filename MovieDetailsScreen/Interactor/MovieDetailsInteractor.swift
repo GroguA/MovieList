fadfac79ddfa94c94ad23d679b9123a0d5d6ac95
GroguA <117662071+GroguA@users.noreply.github.com>
2024-06-20 16:17:45 +0300

    Implement fetching movieby id functionality

--- MovieDetailsScreen/Interactor/MovieDetailsInteractor.swift
+++ MovieDetailsScreen/Interactor/MovieDetailsInteractor.swift
@@ -8,15 +8,64 @@
 import Foundation
 
 protocol IMovieDetailsInteractor {
-    func fetchMovieDetails()
+    func fetchMovieDetails(completion: @escaping (Result<MovieDetailsModel, Error>) -> Void)
 }
 
 final class MovieDetailsInteractor {
     private let serviceLocator = ServiceLocator.shared
+    private let movieId: Int
+    
+    init(movieId: Int) {
+        self.movieId = movieId
+    }
+    
 }
 
 extension MovieDetailsInteractor: IMovieDetailsInteractor {
-    func fetchMovieDetails() {
-        
+    func fetchMovieDetails(completion: @escaping (Result<MovieDetailsModel, Error>) -> Void) {
+        serviceLocator.networkService.getMovieDetailsById(movieId) { result in
+            switch result {
+            case .success(let movie):
+                let mappedMovie = self.mapMovieSchemeToMovieModel(movie)
+                completion(.success(mappedMovie))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
 }
+
+private extension MovieDetailsInteractor {
+    func mapMovieSchemeToMovieModel(_ movie: MovieDetailsResponseScheme) -> MovieDetailsModel {
+        let favoriteMovies = (try? serviceLocator.favoriteMoviesService.fetchFavoriteMovies()) ?? []
+        let isMovieFavorite = favoriteMovies.contains { $0.id == movie.id }
+        let budgetText: String
+        
+        if movie.budget == 0 {
+            budgetText = "not stated"
+        } else if movie.budget / 1_000_000 != 0 {
+            budgetText = "\(movie.budget / 1_000_000) millions $"
+        } else {
+            budgetText = "\(movie.budget / 100_000) thousand $"
+        }
+        
+        let genres = movie.genres.map { $0.name }.joined(separator: ", ")
+        let pathToImage = movie.backdropPath.map { "https://image.tmdb.org/t/p/w500" + $0 }
+        let favoriteMovieText = isMovieFavorite ? "That movie is your favorite one" : "That movie is not your favorite"
+        let formattedPopularity = String(format: "%.2f", (movie.popularity / 1000))
+        
+        return MovieDetailsModel(
+            budget: budgetText,
+            originalLanguage: movie.originalLanguage,
+            releaseDate: movie.releaseDate,
+            status: movie.status,
+            popularity: formattedPopularity,
+            navigationItemTitle: movie.title,
+            genre: genres,
+            pathToImage: pathToImage,
+            isMovieFavorite: favoriteMovieText
+        )
+    }
+}
+
+
--- NetworkLayer/MovieNetworkService.swift
+++ NetworkLayer/MovieNetworkService.swift
@@ -10,6 +10,7 @@ import Foundation
 protocol IMovieNetworkService {
     func getPopularMovies(page: Int, completion: @escaping (Result<[MovieScheme], Error>) -> Void)
     func searchMovieByQuery(_ query: String, completion: @escaping (Result<[MovieScheme], Error>) -> Void)
+    func getMovieDetailsById(_ id: Int, completion: @escaping (Result<MovieDetailsResponseScheme, Error>) -> Void)
 }
 
 final class MovieNetworkService {
@@ -17,69 +18,9 @@ final class MovieNetworkService {
     private let session = URLSession.shared
     private let mapper = JsonDecoderWrapper()
     
-    private func makeURLRequest(endpoint: String, queryItems: [URLQueryItem]) -> URLRequest? {
-        var urlComponents = URLComponents(string: endpoint)
-        urlComponents?.queryItems = queryItems
-        
-        guard let url = urlComponents?.url else {
-            return nil
-        }
-        return URLRequest(url: url)
-    }
-    
-    private func makePopularMoviesRequest(page: Int) -> URLRequest? {
-        let endpoint = "https://api.themoviedb.org/3/movie/popular"
-        let apiKeyItem = URLQueryItem(name: "api_key", value: apiKey)
-        let languageItem = URLQueryItem(name: "language", value: "en-US")
-        let pageItem = URLQueryItem(name: "page", value: "\(page)")
-        
-        let queryItems = [apiKeyItem, languageItem, pageItem]
-        return makeURLRequest(endpoint: endpoint, queryItems: queryItems)
-    }
-    
-    private func makeMovieRequestByQuery(_ query: String) -> URLRequest? {
-        let endpoint = "https://api.themoviedb.org/3/search/movie"
-        let apiKeyItem = URLQueryItem(name: "api_key", value: apiKey)
-        let languageItem = URLQueryItem(name: "language", value: "en-US")
-        let queryItem = URLQueryItem(name: "query", value: query)
-        
-        let queryItems = [apiKeyItem, languageItem, queryItem]
-        return makeURLRequest(endpoint: endpoint, queryItems: queryItems)
-    }
-    
 }
 
 extension MovieNetworkService: IMovieNetworkService {
-    func fetchMovies(with request: URLRequest, completion: @escaping (Result<[MovieScheme], Error>) -> Void) {
-        let completionHandler: (Data?, URLResponse?, Error?) -> Void = { data, response, error in
-            if let error = error {
-                completion(.failure(NetworkErrors.parseError(message: error.localizedDescription)))
-                return
-            }
-            
-            guard let data = data, let httpResponse = response as? HTTPURLResponse else {
-                completion(.failure(NetworkErrors.invalidResponse))
-                return
-            }
-            
-            switch httpResponse.statusCode {
-            case 200..<300:
-                self.mapper.parse(MoviesResponseScheme.self, from: data) { result in
-                    switch result {
-                    case .success(let model):
-                        completion(.success(model.results))
-                    case .failure(let error):
-                        completion(.failure(error))
-                    }
-                }
-            default:
-                completion(.failure(NetworkErrors.statusCodeError(message: HTTPURLResponse.localizedString(forStatusCode: httpResponse.statusCode))))
-            }
-        }
-        let task = session.dataTask(with: request, completionHandler: completionHandler)
-        task.resume()
-    }
-    
     func getPopularMovies(page: Int, completion: @escaping (Result<[MovieScheme], Error>) -> Void) {
         guard let request = makePopularMoviesRequest(page: page) else {
             completion(.failure(NetworkErrors.invalidRequest))
@@ -96,4 +37,96 @@ extension MovieNetworkService: IMovieNetworkService {
         fetchMovies(with: request, completion: completion)
     }
     
+    func getMovieDetailsById(_ id: Int, completion: @escaping (Result<MovieDetailsResponseScheme, any Error>) -> Void) {
+        guard let request = makeMovieDetailsRequestWithId(id) else {
+            completion(.failure(NetworkErrors.invalidRequest))
+            return
+        }
+        fetchMovie(with: request, completion: completion)
+    }
+    
+}
+
+private extension MovieNetworkService {
+    func fetchMovies(with request: URLRequest, completion: @escaping (Result<[MovieScheme], Error>) -> Void) {
+        fetchData(with: request, responseType: MoviesResponseScheme.self) { result in
+            switch result {
+            case .success(let model):
+                completion(.success(model.results))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func fetchMovie(
+        with request: URLRequest,
+        completion: @escaping (Result<MovieDetailsResponseScheme, Error>) -> Void
+    ) {
+        fetchData(with: request, responseType: MovieDetailsResponseScheme.self, completion: completion)
+    }
+    
+    func fetchData<T: Decodable>(with request: URLRequest, responseType: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
+        let task = session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(NetworkErrors.parseError(message: error.localizedDescription)))
+                return
+            }
+            
+            guard let data = data, let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(NetworkErrors.invalidResponse))
+                return
+            }
+            
+            sleep(1)
+            
+            switch httpResponse.statusCode {
+            case 200..<300:
+                self.mapper.parse(responseType, from: data, completion: completion)
+            default:
+                let errorMessage = HTTPURLResponse.localizedString(forStatusCode: httpResponse.statusCode)
+                completion(.failure(NetworkErrors.statusCodeError(message: errorMessage)))
+            }
+        }
+        task.resume()
+    }
+    
+    func makeURLRequest(endpoint: String, queryItems: [URLQueryItem]) -> URLRequest? {
+        var urlComponents = URLComponents(string: endpoint)
+        urlComponents?.queryItems = queryItems
+        
+        guard let url = urlComponents?.url else {
+            return nil
+        }
+        return URLRequest(url: url)
+    }
+    
+    func makePopularMoviesRequest(page: Int) -> URLRequest? {
+        let endpoint = "https://api.themoviedb.org/3/movie/popular"
+        let apiKeyItem = URLQueryItem(name: "api_key", value: apiKey)
+        let languageItem = URLQueryItem(name: "language", value: "en-US")
+        let pageItem = URLQueryItem(name: "page", value: String(page))
+        
+        let queryItems = [apiKeyItem, languageItem, pageItem]
+        return makeURLRequest(endpoint: endpoint, queryItems: queryItems)
+    }
+    
+    func makeMovieRequestByQuery(_ query: String) -> URLRequest? {
+        let endpoint = "https://api.themoviedb.org/3/search/movie"
+        let apiKeyItem = URLQueryItem(name: "api_key", value: apiKey)
+        let languageItem = URLQueryItem(name: "language", value: "en-US")
+        let queryItem = URLQueryItem(name: "query", value: query)
+        
+        let queryItems = [apiKeyItem, languageItem, queryItem]
+        return makeURLRequest(endpoint: endpoint, queryItems: queryItems)
+    }
+    
+    func makeMovieDetailsRequestWithId(_ id: Int) -> URLRequest? {
+        let endpoint = "https://api.themoviedb.org/3/movie/\(id)"
+        let apiKeyItem = URLQueryItem(name: "api_key", value: apiKey)
+        let languageItem = URLQueryItem(name: "language", value: "en-US")
+        
+        let queryItems = [apiKeyItem, languageItem]
+        return makeURLRequest(endpoint: endpoint, queryItems: queryItems)
+    }
 }
